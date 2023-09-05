b298c3e0bf6cfbf9a8a2996ae8bab1d5eb52835f
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-09-05 22:37:28 +0530

    UserData, import sampleData , title switch toggle

---
+++ src/components/User.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import { Switch } from '@headlessui/react';
+import SampleData from "./SampleData";
+
+function User() {
+  const [enabled, setEnabled] = useState(false);
+  
+  return <div>
+      <div class="px-4 py-8 flex items-center">
+          <p class="text-2xl leading-3 text-gray-900 flex items-center">
+              <span class="font-semibold">Hey Mariana -</span>
+              <span class="font-normal text-base text-gray-500 ml-2">here's what's happening</span>
+          </p>
+          <Switch
+              checked={enabled}
+              onChange={setEnabled}
+              className={`${enabled ? 'bg-gray-700' : 'bg-blue-500'}
+      relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75 ml-4`}
+          >
+              <span className="sr-only">Use setting</span>
+              <span
+                  aria-hidden="true"
+                  className={`${enabled ? 'translate-x-9' : 'translate-x-0'}
+        pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+              />
+          </Switch>
+          <p class="text-2xl ml-4 leading-3 text-gray-900 flex items-center">
+              <span class="font-semibold">DEMO DATA </span>
+          </p>
+      </div>
+
+      <div className="flex flex-row gap-4 w-full">
+          <SampleData />
+      </div>
+  </div>;
+}
+
+export default User;
